00da55e7c07dc717c2aa66f25a0e3f6ebb42be85
bayudani <bayu22122017@gmail.com>
2026-03-10 13:40:34 +0700

    fix: Correct PDO MySQL SSL attribute reference in database configuration and update Livewire script routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
+use Livewire\Livewire; 
+use Illuminate\Support\Facades\Route;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,6 +23,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Mengubah URL script agar tidak berakhiran ".js"
+        // Vercel tidak akan memblokirnya lagi dan akan meneruskannya ke Laravel
+        Livewire::setScriptRoute(function ($handle) {
+            return Route::get('/livewire/livewire-script', $handle); 
+        });
+
+        // Pastikan update route juga aman
+        Livewire::setUpdateRoute(function ($handle) {
+            return Route::post('/livewire/update', $handle);
+        });
+
         if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
